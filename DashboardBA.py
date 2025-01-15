aa1db2c57661c2742d63025df66724cb47a8258b
Makem30 <108522109+Makem30@users.noreply.github.com>
2025-01-15 19:46:47 +0100

    Update DashboardBA.py

--- DashboardBA.py
+++ DashboardBA.py
@@ -75,6 +75,8 @@ with col4:
 
 # Graphique des ventes quotidiennes 
 # Convertir la colonne 'date' en datetime
+import pandas as pd
+
 data = pd.read_csv('data_dashboard_large - data_dashboard_large.csv')
 data['date'] = pd.to_datetime(data['date'])
 
