d2ab733c9566e7e8832b5ee32a5ced03962c6e03
Makem30 <108522109+Makem30@users.noreply.github.com>
2025-01-15 23:47:59 +0100

    Update DashboardBA.py

--- DashboardBA.py
+++ DashboardBA.py
@@ -178,3 +178,14 @@ chart = alt.Chart(category_sales).mark_bar().encode(
 
 # Afficher le graphique sur Streamlit
 st.altair_chart(chart, use_container_width=True)
+#-----------------------------------------------------------------------------------
+# Grouper par catégorie et produit, puis calculer la quantité vendue
+product_sales = data.groupby(['Categorie_Produit', 'Montant'])['Quantite'].sum().reset_index()
+
+# Trier par quantité vendue et obtenir le top 5 pour chaque catégorie
+top_products = product_sales.groupby('Categorie_Produit').apply(
+    lambda x: x.sort_values('Quantite', ascending=False).head(5)
+).reset_index(drop=True)
+
+# Afficher le tableau sur Streamlit
+st.table(top_products)
