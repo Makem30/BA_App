91a87ed5e150d39e9849f8a1b93ee267dca82076
Makem30 <108522109+Makem30@users.noreply.github.com>
2025-01-15 14:05:32 +0100

    Update DashboardBA.py

--- DashboardBA.py
+++ DashboardBA.py
@@ -1,1 +1,9 @@
+import streamlit as st
+import pandas as pd
+data = pd.read_csv('data_dashboard_large - data_dashboard_large.csv')
 
+# Calculer le total des ventes
+total_ventes = data['Montant'].sum()
+
+# Afficher le KPI dans Streamlit
+st.metric(label="Total des ventes", value=f"{total_ventes:.2f} €")
