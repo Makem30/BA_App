499014701dfd737e4a9a333241be6236c90ad152
Makem30 <108522109+Makem30@users.noreply.github.com>
2025-01-15 14:34:15 +0100

    Update DashboardBA.py

--- DashboardBA.py
+++ DashboardBA.py
@@ -9,22 +9,58 @@ total_ventes = data['Montant'].sum()
 
 #-----------------------------------------------------------------------------------------------
 # Calculer les KPI
-nombre_total_transactions = len(data)
-montant_moyen_transaction = data['Montant'].mean()
-satisfaction_client_moyenne = data['Satisfaction_Client'].mean()
+# nombre_total_transactions = len(data)
+# montant_moyen_transaction = data['Montant'].mean()
+# satisfaction_client_moyenne = data['Satisfaction_Client'].mean()
 
-# Afficher les KPI dans Streamlit
-col1, col2, col3, col4 = st.columns([1.5,2,1.5,1.5], gap="large")  # Créer 3 colonnes pour afficher les KPI côte à côte
+# # Afficher les KPI dans Streamlit
+# col1, col2, col3, col4 = st.columns([1.5,2,1.5,1.5], gap="large")  # Créer 3 colonnes pour afficher les KPI côte à côte
 
+
+# with col1:
+#     st.metric(label="Nombre total de transactions", value=nombre_total_transactions)
+
+# with col2:
+#     st.metric(label="Montant moyen par transaction", value=f"{montant_moyen_transaction:.2f} €")
+
+# with col3:
+#     st.metric(label="Satisfaction client moyenne", value=f"{satisfaction_client_moyenne:.2f}")
+    
+# with col4:
+#     st.metric(label="Total des ventes", value=f"{total_ventes:.2f} €")
+
+#-------------------------------------------------------------------------------------------
+st.markdown(
+    """
+    <style>
+    .metric-container {
+        background-color: white;
+        border: 1px solid #ccc;
+        padding: 20px;
+        border-radius: 5px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# Afficher les KPI dans les carrés
+col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.metric(label="Nombre total de transactions", value=nombre_total_transactions)
+    with st.container():
+        st.markdown('<div class="metric-container">', unsafe_allow_html=True)
+        st.metric(label="Nombre total de transactions", value=nombre_total_transactions)
+        st.markdown('</div>', unsafe_allow_html=True)
 
 with col2:
-    st.metric(label="Montant moyen par transaction", value=f"{montant_moyen_transaction:.2f} €")
+    with st.container():
+        st.markdown('<div class="metric-container">', unsafe_allow_html=True)
+        st.metric(label="Montant moyen par transaction", value=f"{montant_moyen_transaction:.2f} €")
+        st.markdown('</div>', unsafe_allow_html=True)
 
 with col3:
-    st.metric(label="Satisfaction client moyenne", value=f"{satisfaction_client_moyenne:.2f}")
-    
-with col4:
-    st.metric(label="Total des ventes", value=f"{total_ventes:.2f} €")
+    with st.container():
+        st.markdown('<div class="metric-container">', unsafe_allow_html=True)
+        st.metric(label="Satisfaction client moyenne", value=f"{satisfaction_client_moyenne:.2f}")
+        st.markdown('</div>', unsafe_allow_html=True)
