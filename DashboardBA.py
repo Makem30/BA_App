2d442242aedb069eb34e421bbf0db31b0ed70e35
Makem30 <108522109+Makem30@users.noreply.github.com>
2025-01-15 23:05:35 +0100

    Update DashboardBA.py

--- DashboardBA.py
+++ DashboardBA.py
@@ -123,8 +123,6 @@ with col2:
     store_sales = data.groupby('Magasin').agg(
         Total_ventes=('Montant', 'sum'),
         Nb_transactions=('Quantite', 'nunique')
-    ).properties(
-        title="Ventes totales et nombre de transactions par magasin"
     ).reset_index()
     
     # Afficher le tableau sur Streamlit
