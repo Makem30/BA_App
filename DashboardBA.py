752136bea30a0da468b9282ba4c2bab9f411685d
Makem30 <108522109+Makem30@users.noreply.github.com>
2025-01-15 14:13:02 +0100

    Update DashboardBA.py

--- DashboardBA.py
+++ DashboardBA.py
@@ -7,3 +7,20 @@ total_ventes = data['Montant'].sum()
 
 # Afficher le KPI dans Streamlit
 st.metric(label="Total des ventes", value=f"{total_ventes:.2f} €")
+#-----------------------------------------------------------------------------------------------
+# Calculer les KPI
+nombre_total_transactions = len(data)
+montant_moyen_transaction = data['Montant'].mean()
+satisfaction_client_moyenne = data['Satisfaction_Client'].mean()
+
+# Afficher les KPI dans Streamlit
+col1, col2, col3 = st.columns(3)  # Créer 3 colonnes pour afficher les KPI côte à côte
+
+with col1:
+    st.metric(label="Nombre total de transactions", value=nombre_total_transactions)
+
+with col2:
+    st.metric(label="Montant moyen par transaction", value=f"{montant_moyen_transaction:.2f} €")
+
+with col3:
+    st.metric(label="Satisfaction client moyenne", value=f"{satisfaction_client_moyenne:.2f}")
